8b2909c575a0773e6f3441d316d346d3112d4462
Tidic <tidic84@gmail.com>
2021-11-26 23:04:15 +0100

    db connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 require('./models/dbConfig')
 
 app.listen(5500, () => console.log('Servver started: 5050'));
--- models/dbConfig.js
+++ models/dbConfig.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    'mongodb://localhost:27017/free-games-api',
+    `${process.env.MONGODB_URL}`,
     { useNewUrlParser: true , useUnifiedTopology: true },
     (err) => {
         if (!err)console.log('MongoDB connection succeeded.');
