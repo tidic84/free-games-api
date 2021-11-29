d4c946f172ca7bc6c02489a5d0be5bb7f5eaa4d8
Tidic <tidic84@gmail.com>
2021-11-29 20:40:45 +0100

    maj db

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 require('dotenv').config();
 require('./models/dbConfig');
 const gamesRoutes = require('./routes/controller');
+const bodyParser = require('body-parser');
 
-app.use('/', gamesRoutes);
+app.use(bodyParser.json());
+app.use('/games', gamesRoutes);
 
 app.listen(5500, () => console.log('Servver started: 5500'));
